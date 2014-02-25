5512bdb578c5f8aaea743c963bfc3b5662a7c9de
harebu <bugu1986@126.com>
2014-02-25 09:44:16 +0800

    add include & exclude

--- src/main/groovy/io/buhe/GatlingPlugin.groovy
+++ src/main/groovy/io/buhe/GatlingPlugin.groovy
@@ -3,12 +3,63 @@ package io.buhe;
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import java.util.regex.Pattern
+
 class GatlingPlugin implements Plugin<Project> {
+
+    static Pattern createPatternFromList(List<String> list) {
+        if (list == null || list.size() == 0)
+            return null;
+        StringBuilder sb = new StringBuilder(500);
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i < list.size() - 1)
+                sb.append("|");
+        }
+        return Pattern.compile(sb.toString());
+    }
+
+    static boolean check(String clazz,Pattern blackListPattern,Pattern whiteListPattern){
+        if (inWhiteList(clazz,whiteListPattern)) {
+            println "---- "+clazz + "  in white list. ----"
+            return true;
+        } else {
+            if (inBlackList(clazz,blackListPattern)) {
+                println "---- "+clazz + "  in black list. ----"
+                return false;
+            } else {
+                println "---- "+clazz + "  not in black and white list. ----"
+                return true;
+            }
+        }
+    }
+
+    static  boolean inBlackList(String path, Pattern blackListPattern) {
+        if (blackListPattern == null)
+            return false;
+        return blackListPattern.matcher(path).matches();
+    }
+
+    static boolean inWhiteList(String path, Pattern whiteListPattern) {
+        if (whiteListPattern == null)
+            return false;
+        return whiteListPattern.matcher(path).matches();
+    }
+
+
     void apply(Project project) {
 
-        project.task('gatling').dependsOn("compileTestScala") << {
-            println "Current time is " + new Date();
+        project.extensions.create("gatling", GatlingPluginExtension);
 
+        project.task('gatling').dependsOn("compileTestScala") << {
+            println 'Include ' + project.gatling.include
+            println 'Exclude ' + project.gatling.exclude
+
+            def whiteListPattern = createPatternFromList(Arrays.asList(project.gatling.include));
+            def blackListPattern = createPatternFromList(Arrays.asList(project.gatling.exclude));
+
+            println 'Include ' + whiteListPattern
+            println 'Exclude ' + blackListPattern
 
             logger.lifecycle(" ---- Executing all Gatling scenarios from: ${project.sourceSets.test.output.classesDir} ----")
             project.sourceSets.test.output.classesDir.eachFileRecurse { file ->
@@ -17,20 +68,23 @@ class GatlingPlugin implements Plugin<Project> {
                     logger.debug("Tranformed file ${file} into")
                     def gatlingScenarioClass = (file.getPath() - (project.sourceSets.test.output.classesDir.getPath() + File.separator) - '.class')
                             .replace(File.separator, '.')
-
-                    logger.debug("Tranformed file ${file} into scenario class ${gatlingScenarioClass}")
-                    project.javaexec {
-                        // I do not use this so
-                        main = 'com.excilys.ebi.gatling.app.Gatling'
-                        classpath = project.sourceSets.test.output + project.sourceSets.test.runtimeClasspath
-                        report = project.buildDir.getAbsolutePath()+'/reports/gatling';
-                        args  '-sbf',
-                                project.sourceSets.test.output.classesDir,
-                                '-s',
-                                gatlingScenarioClass,
-                                '-rf',
-                                report
+                    if(check(gatlingScenarioClass,blackListPattern,whiteListPattern)){
+                        logger.debug("Tranformed file ${file} into scenario class ${gatlingScenarioClass}")
+                        project.javaexec {
+                            // I do not use this so
+                            main = 'com.excilys.ebi.gatling.app.Gatling'
+                            classpath = project.sourceSets.test.output + project.sourceSets.test.runtimeClasspath
+                            report = project.buildDir.getAbsolutePath()+'/reports/gatling';
+                            args  '-sbf',
+                                    project.sourceSets.test.output.classesDir,
+                                    '-s',
+                                    gatlingScenarioClass,
+                                    '-rf',
+                                    report
+                        }
                     }
+
+
                 }
             }
 
--- src/main/groovy/io/buhe/GatlingPluginExtension.groovy
+++ src/main/groovy/io/buhe/GatlingPluginExtension.groovy
@@ -6,4 +6,6 @@ package io.buhe
  * Time: 9:29 AM
  */
 class GatlingPluginExtension {
+    def  include = null;
+    def  exclude = null;
 }
